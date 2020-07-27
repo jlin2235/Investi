aee131786e33ef5e25badb6b9bd33d62ac23ac59
Jlin2235 <Jlin2235@gmail.com>
2020-07-27 17:15:25 -0400

    Heroku setup

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -5,7 +5,6 @@ import configureStore from './store/store';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    // debugger
     const root = document.getElementById('root');
     const store = configureStore();
 
