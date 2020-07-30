48e0bad7786d1878974e14a5d3dd5be442b7c742
Jlin2235 <Jlin2235@gmail.com>
2020-07-30 15:24:20 -0400

    login page css complete

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -9,6 +9,7 @@ import { Route, Switch, Link } from "react-router-dom";
 import NavBarContainer from './nav_bar/nav_bar_container'
 import LogInFormContainer from './session_form/login_form_container'
 import SignUpFormContainer from './session_form/signup_form_container'
+import { AuthRoute, ProtectedRoute } from './../util/route_util'
 
 const App = () => (
     <div>
@@ -21,8 +22,8 @@ const App = () => (
 
 
             <Route exact path="/" component={NavBarContainer} />
-            <Route exact path="/login" component={LogInFormContainer} />
-            <Route exact path="/signup" component={SignUpFormContainer} /> 
+            <AuthRoute exact path="/login" component={LogInFormContainer} />
+            <AuthRoute exact path="/signup" component={SignUpFormContainer} /> 
         </Switch>
     </div>
 );
